f7cafd4e76b3d9d41aa8b7871656e88fe8f0d58f
Matteo Ferrando <matteo.ferrando2@gmail.com>
2019-04-05 10:50:30 -0400

    remove trigger for account_has_at_least_one_member

--- db/revert/functions/trigger_function_account_has_at_least_one_member.sql
+++
@@ -1,7 +0,0 @@
--- Revert i:functions/trigger_function_account_has_at_least_one_member from pg
-
-BEGIN;
-
-DROP FUNCTION account_has_at_least_one_member();
-
-COMMIT;
--- db/revert/tables/members/trigger_account_has_at_least_one_member.sql
+++
@@ -1,7 +0,0 @@
--- Revert i:tables/members/trigger_account_has_at_least_one_member from pg
-
-BEGIN;
-
-DROP TRIGGER account_has_at_least_one_member ON app.members;
-
-COMMIT;
