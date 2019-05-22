837b2d5d8d58c8f176b8e799f6bd027e2fa925b1
Matteo Ferrando <matteo.ferrando2@gmail.com>
2019-05-22 19:46:56 -0400

    products service

---
+++ web/src/services/ProductsService.ts
@@ -0,0 +1,42 @@
+import { axiosPG, preferHeader } from './Request';
+import { authHeader } from './Auth';
+
+export interface Product {
+  sku: string;
+  description: string;
+  created_at: string;
+  account_id: string;
+}
+
+export default class ProductsService {
+
+  static async fetchAllProducts(order: keyof Product = 'created_at') {
+    return await axiosPG.get<Product[]>('/products', {
+      headers: authHeader(),
+      params: { order }
+    });
+  }
+
+  static async fetchProduct(sku: Product['sku']) {
+    const res= await axiosPG.get<Product[]>('/products', {
+      headers: authHeader(),
+      params: {
+        sku: 'eq.' + sku
+      }
+    });
+
+    return res.data[0];
+  }
+
+  static async postProduct(payload: Partial<Product>) {
+    const res = await axiosPG.post<Account[]>('/products', payload, {
+      headers: {
+        ...authHeader(),
+        ...preferHeader('representation')
+      }
+    });
+
+    return res.data[0];
+  }
+
+}
