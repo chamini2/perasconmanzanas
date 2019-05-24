26a030de18362ede592a91a483e1839f85117899
Matteo Ferrando <matteo.ferrando2@gmail.com>
2019-05-24 17:53:40 -0400

    use AgGrid for products list

--- web/src/containers/ProductsIndex.tsx
+++ web/src/containers/ProductsIndex.tsx
@@ -4,11 +4,13 @@ import isLoggedInGuard from '../wrappers/isLoggedInGuard';
 import Header, { headerContainerStyle, headerSiblingStyle } from '../components/Header';
 import { Link } from 'react-router-dom';
 import ProductsService, { Product } from '../services/ProductsService';
-import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
 import formatd from 'date-fns/format';
 import { TIMESTAMP_FORMAT, DATE_LOCALE } from '../constants';
 import * as Paths from '../Paths';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-bootstrap.css';
+import { AgGridReact } from 'ag-grid-react';
 
 interface State {
   products?: Product[];
@@ -32,6 +34,7 @@ class ProductsIndex extends Component<{}, State> {
     return <ListGroupItem
       as={Link}
       to={Paths.ProductDetails(product.sku, product.description)}
+      key={product.sku}
     >
       <div>{product.sku}</div>
       <div>{product.description}</div>
@@ -39,8 +42,29 @@ class ProductsIndex extends Component<{}, State> {
     </ListGroupItem>;
   }
 
-  render() {
+  renderProducts() {
     const { products } = this.state;
+    if (!products) {
+      return 'Cargando...';
+    }
+
+    return <div
+      className='ag-theme-bootstrap'
+      style={{ height: '500px' }}
+    >
+      <AgGridReact
+        columnDefs={[
+          { headerName: 'SKU', field:'sku', filter: 'text', width: 100 },
+          { headerName: 'Description', field:'description', filter: 'text', width: 400 },
+          { field:'created_at', hide: true }
+        ]}
+        sortingOrder={['created_at']}
+        rowData={products}
+      />
+    </div>;
+  }
+
+  render() {
     return <div style={headerContainerStyle} className='ProductsIndex container'>
       <Header />
 
@@ -54,10 +78,7 @@ class ProductsIndex extends Component<{}, State> {
           </Link>
         </div>
 
-        {/* TODO: Use <AgGridReact> */}
-        <ListGroup>
-        { products ? products.map(this.renderProduct) : 'Cargando productos...' }
-        </ListGroup>
+        {this.renderProducts()}
 
         {/* TODO: Check it out */}
         <h4>Pendiente: ProductIndex</h4>
