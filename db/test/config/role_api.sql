ff626820c021f87fb2c3cbbc1994a0396e360132
Matteo Ferrando <matteo.ferrando2@gmail.com>
2019-06-25 18:41:42 -0400

    make tests pass and fix production docker-compose

--- db/test/config/role_api.sql
+++ db/test/config/role_api.sql
@@ -1,13 +1,18 @@
 BEGIN;
-SELECT plan(5);
+SELECT plan(8);
 
 SELECT has_role('api');
 
 SELECT schema_privs_are('auth', 'api', ARRAY['USAGE']);
-SELECT table_privs_are('auth', 'users', 'api', ARRAY['SELECT', 'INSERT']);
+
+SELECT table_privs_are('auth', 'users', 'api', ARRAY['SELECT', 'INSERT', 'UPDATE']);
 
 SELECT schema_privs_are('app', 'api', ARRAY['USAGE']);
+
 SELECT table_privs_are('app', 'users', 'api', ARRAY['SELECT', 'INSERT']);
+SELECT table_privs_are('app', 'accounts', 'api', ARRAY['SELECT']);
+SELECT table_privs_are('app', 'members', 'api', ARRAY['SELECT', 'INSERT']);
+SELECT table_privs_are('app', 'invites', 'api', ARRAY['SELECT', 'UPDATE']);
 
 SELECT finish();
 ROLLBACK;
