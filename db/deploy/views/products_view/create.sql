df31b819d5072ae8cf7fdf9f380618893cb7725c
Matteo Ferrando <matteo.ferrando2@gmail.com>
2019-06-26 10:46:28 -0400

    handle no movements for products_view

--- db/deploy/views/products_view/create.sql
+++ db/deploy/views/products_view/create.sql
@@ -8,9 +8,9 @@ CREATE VIEW app.products_view AS
     p.account_id,
     p.description,
     p.created_at,
-    sum(m.quantity) AS stock
+    coalesce(sum(m.quantity), 0) AS stock
   FROM app.products p
-    INNER JOIN app.movements m ON ((m.product_sku, m.account_id) = (p.sku, p.account_id))
+    LEFT JOIN app.movements m ON ((m.product_sku, m.account_id) = (p.sku, p.account_id))
   GROUP BY (p.sku, p.account_id);
 
 COMMIT;
--- db/test/views/products_view/create.sql
+++ db/test/views/products_view/create.sql
@@ -1,7 +1,32 @@
 BEGIN;
-SELECT plan(1);
+SELECT plan(2);
+
+INSERT INTO app.users (id, username, email, full_name)
+VALUES (1, 'john', 'john@example.com', 'John Doe');
+
+INSERT INTO app.accounts (id, name, owner_id)
+VALUES ('shorts', 'Shorts Store', 1);
+
+INSERT INTO app.products (sku, account_id, description)
+VALUES
+    ('L-GRAY', 'shorts', 'The gray large ones'),
+    ('M-GRAY', 'shorts', 'The gray medium ones');
+
+INSERT INTO app.movements (product_sku, account_id, user_id, quantity, description)
+VALUES
+    ('L-GRAY', 'shorts', 1, 20, 'Inventory start');
 
 SELECT has_view('app', 'products_view', 'View products_view should exist');
+
+-- (shorts, 2, false)
+SET "request.jwt.claim.user" TO 2;
+SET "request.jwt.claim.account" TO 'shorts';
+SET SESSION AUTHORIZATION web_user;
+SELECT results_eq(
+    $$SELECT sku::text, stock::integer FROM app.products_view ORDER BY sku$$,
+    $$VALUES ('L-GRAY', 20), ('M-GRAY', 0)$$,
+    'Should SELECT products even if no movement is associated'
+);
 
 SELECT finish();
 ROLLBACK;
