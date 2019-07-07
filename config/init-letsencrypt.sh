549eb20bf8b709141c11a32403962818c00e3fb5
Matteo Ferrando <matteo.ferrando2@gmail.com>
2019-07-07 11:37:08 -0400

    add metabase container name and fix init-letsencrypt data path

--- config/init-letsencrypt.sh
+++ config/init-letsencrypt.sh
@@ -5,7 +5,7 @@
 
 domains=(api.perasconmanzanas.com server.perasconmanzanas.com metabase.perasconmanzanas.com)
 rsa_key_size=4096
-data_path="./deploy/data/certbot"
+data_path="./config/data/certbot"
 email="" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 docker_file=docker-compose.pro.yml
